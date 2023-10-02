d7601efa620b8cb9e7e92ff4a805cdd690ba8665
Malú <130713333+malummartiins@users.noreply.github.com>
2023-10-02 19:01:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# api
+ API rest com Spring Boot, desenvolvido com Flyway como ferramenta de Migrations da API,  e CRUDs utilizando o banco de dados MySQL
---
+++ src/main/java/med/voll/api/medico/DadosListageMedico.java
@@ -0,0 +1,8 @@
+package med.voll.api.medico;
+
+public record DadosListageMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
+
+    public  DadosListageMedico (Medico medico){
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+    }
+}
