47697f1fd22da5c625ff0ab38afef5dffb55b1d9
Malú <130713333+malummartiins@users.noreply.github.com>
2023-10-17 13:49:55 -0300

    update consulta

--- src/main/java/med/voll/api/controller/ConsultaController.java
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -3,9 +3,11 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultas;
 import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
 import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
 import med.voll.api.domain.medico.DadosDetalhamentomedico;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("consultas")
 public class ConsultaController {
+    @Autowired
+    private AgendaDeConsultas agenda;
 
     @PostMapping
     @Transactional
     public ResponseEntity agendar(@RequestBody @Valid DadosAgendamentoConsulta dados){
+
+        agenda.agendar(dados);
 
         return ResponseEntity.ok(new DadosDetalhamentoConsulta(null, null, null, null));
 
---
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,33 @@
+package med.voll.api.domain.consulta;
+
+import med.voll.api.domain.medico.MedicoRepository;
+import med.voll.api.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    //aqui ficam as regras de negócio da aplicação
+    //o objetivo é salvar uma consulta no BD
+
+    public void agendar(DadosAgendamentoConsulta dados){
+
+
+        
+        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var consulta = new Consulta(null,medico, paciente, dados.data());
+        consultaRepository.save(consulta);
+    }
+
+}
