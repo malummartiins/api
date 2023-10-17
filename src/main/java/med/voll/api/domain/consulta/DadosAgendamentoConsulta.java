46319b69defd7b01e7d0e91fcb718ec7f47bce74
Malú <130713333+malummartiins@users.noreply.github.com>
2023-10-17 09:33:39 -0300

    update - começando consulta

---
+++ src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -0,0 +1,19 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record DadosAgendamentoConsulta(
+    Long idMedico,
+
+    @NotNull
+    Long idPaciente,
+
+    @NotNull
+    @Future  //anotação para dizer que a data tem que ser no futuro
+    LocalDateTime data){
+
+}
+
