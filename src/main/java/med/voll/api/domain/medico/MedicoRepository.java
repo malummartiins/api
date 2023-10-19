87dff65f435de7008b078368d392d57cf22090a4
Malú <130713333+malummartiins@users.noreply.github.com>
2023-10-19 18:17:01 -0300

    update processos

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -38,5 +38,5 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             where
             m.id = :id
             """)
-    Boolean findAtivoById(Long idMedico);
+    Boolean findAtivoById(Long id);
 }
